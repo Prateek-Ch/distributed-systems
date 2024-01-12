0069ec66c5cf98ff0f2ad709c0dd96217cf860a6
PrateekCh <prateek.chaturvedi216@gmail.com>
2024-01-12 18:46:52 +0100

    dynamic discovery of host maybe?

--- client/client.py
+++ client/client.py
@@ -1,19 +1,43 @@
-import socket, pickle
+import socket
+import pickle
+import threading
 
 HOST = '192.168.56.1'
 PORT = 9090
 AVAILABLE = 'available'
 ACK = 'ack'
 
+server_available_event = threading.Event()
+
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(pickle.dumps(AVAILABLE), (HOST,PORT))
+
+def dynamic_host_discovery():
+    print("waiting for server..")
+    discovery_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    discovery_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    discovery_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    discovery_socket.bind(("", 37020))
+
+    while True:
+        data, _ = discovery_socket.recvfrom(1024)
+        message = pickle.loads(data)
+        if message == AVAILABLE:
+            print(f"Server available at {HOST}:{PORT}")
+            client.sendto(pickle.dumps(AVAILABLE), (HOST,PORT))
+            server_available_event.set()
+
+discovery_thread = threading.Thread(target=dynamic_host_discovery, daemon=True)
+discovery_thread.start()
+
+# Wait for the server discovery before proceeding
+server_available_event.wait()
 
 while True:
-    data, _ = client.recvfrom(4096) 
-    sentence = pickle.loads(data)
-    print(sentence)
-    # TODO: replace with logic of ngram
-    if sentence != ACK and sentence != AVAILABLE:
-        words = sentence.split()
-        result_dict = {'ngram': len(words)}
-        client.sendto(pickle.dumps(result_dict), (HOST,PORT))
+    data, _ = client.recvfrom(4096)
+    if data:
+        sentence = pickle.loads(data)
+        # TODO: replace with logic of ngram
+        if sentence != ACK and sentence != AVAILABLE:
+            words = sentence.split()
+            result_dict = {'ngram': len(words)}
+            client.sendto(pickle.dumps(result_dict), (HOST, PORT))
--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-import socket, threading, pickle
+import socket, threading, pickle, time
 from helper import create_segments
 
 HOST = socket.gethostbyname(socket.gethostname())
@@ -31,6 +31,13 @@ def calculate_result():
             result = sum(results)
             print(f"Final result: {result}")
             results.clear()
+
+def dynamic_host_discovery():
+    while True:
+        broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print("sending broadcast message")
+        broadcast_socket.sendto(pickle.dumps(AVAILABLE), ('192.168.1.255', 37020))
+        time.sleep(5)
             
           
 def start():
@@ -38,8 +45,11 @@ def start():
     # TODO: figure out if starting threads like this is safe and check for race conditions
     input_thread = threading.Thread(target=handle_input, daemon= True)
     results_thread = threading.Thread(target=calculate_result, daemon=True)
+    discovery_thread = threading.Thread(target=dynamic_host_discovery, daemon=True)
+    
     input_thread.start()
     results_thread.start()
+    discovery_thread.start()
     
     while True:
         data, address = server.recvfrom(1024)
@@ -48,6 +58,7 @@ def start():
         server.sendto(pickle.dumps(ACK), address)
         if message == AVAILABLE:
             addresses.append(address)
+            print(f"adddreses list: {addresses}")
         elif type(message) == dict and 'ngram' in message.keys():
             results.append(int(message['ngram'])) 
 print("Server is starting...")
